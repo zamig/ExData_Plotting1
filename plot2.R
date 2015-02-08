a927c64065d82d30612f0923d51e64b7e24e9732
zamig <zami_g@yahoo.com>
2015-02-08 23:23:45 +0200

    adding comments

--- plot2.R
+++ plot2.R
@@ -5,7 +5,11 @@ require(sqldf)
 # The data can be download from  
 # https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
 
-# Loading the required dates from the data file
+# Loading the required dates from the data file.
+# Using "read.csv2.sql" to load only the needed dates.
+# read.cvs.sql does not handle the missig values in the file
+# but the given data and needed dates have no missing values
+# and hence the use of this method does not cause any problem.
 powerData <- read.csv2.sql("household_power_consumption.txt", 
                         sql = "select * from file where Date = '1/2/2007' or
                         Date = '2/2/2007'")
